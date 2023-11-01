353287e4578e71327c52ae19ce0faedcf62b965e
James Edington Administrator <james.edington@uah.edu>
2023-11-01 17:31:29 -0500

    Use bytes for const char[] arguments

--- main.py
+++ main.py
@@ -23,13 +23,13 @@ def _impl_randombytes(output, n, *, ffi=_mceliece6960119f.ffi):
 def demo_keypair():
 	"""MCELIECE6960119F - Returns *publickey*, *secretkey*.
 	"""
+
 	pk = bytearray(_mceliece6960119f.lib.PQCLEAN_MCELIECE6960119F_CLEAN_CRYPTO_PUBLICKEYBYTES)
-	_pk = _mceliece6960119f.ffi.from_buffer(pk)
-
 	sk = bytearray(_mceliece6960119f.lib.PQCLEAN_MCELIECE6960119F_CLEAN_CRYPTO_SECRETKEYBYTES)
-	_sk = _mceliece6960119f.ffi.from_buffer(sk)
 
-	_mceliece6960119f.lib.PQCLEAN_MCELIECE6960119F_CLEAN_crypto_kem_keypair(_pk, _sk)
+	_mceliece6960119f.lib.PQCLEAN_MCELIECE6960119F_CLEAN_crypto_kem_keypair(
+	    _mceliece6960119f.ffi.from_buffer(pk),
+	    _mceliece6960119f.ffi.from_buffer(sk))
 
 	return bytes(pk), bytes(sk)
 
@@ -39,15 +39,12 @@ def demo_kem_enc(pk):
 	"""
 
 	ciphertext = bytearray(_mceliece6960119f.lib.PQCLEAN_MCELIECE6960119F_CLEAN_CRYPTO_CIPHERTEXTBYTES)
-	_c = _mceliece6960119f.ffi.from_buffer(ciphertext)
-
 	shared_key = bytearray(_mceliece6960119f.lib.PQCLEAN_MCELIECE6960119F_CLEAN_CRYPTO_BYTES)
-	_key = _mceliece6960119f.ffi.from_buffer(shared_key)
 
-	_pk = _mceliece6960119f.ffi.from_buffer(pk)
-	assert len(_pk) == _mceliece6960119f.lib.PQCLEAN_MCELIECE6960119F_CLEAN_CRYPTO_PUBLICKEYBYTES
-
-	_mceliece6960119f.lib.PQCLEAN_MCELIECE6960119F_CLEAN_crypto_kem_enc(_c, _key, _pk)
+	_mceliece6960119f.lib.PQCLEAN_MCELIECE6960119F_CLEAN_crypto_kem_enc(
+	    _mceliece6960119f.ffi.from_buffer(ciphertext),
+	    _mceliece6960119f.ffi.from_buffer(shared_key),
+	    pk)
 
 	return bytes(shared_key), bytes(ciphertext)
 
@@ -57,15 +54,11 @@ def demo_kem_dec(sk, ciphertext):
 	"""
 
 	shared_key = bytearray(_mceliece6960119f.lib.PQCLEAN_MCELIECE6960119F_CLEAN_CRYPTO_BYTES)
-	_key = _mceliece6960119f.ffi.from_buffer(shared_key)
 
-	_c = _mceliece6960119f.ffi.from_buffer(ciphertext)
-	assert len(_c) == _mceliece6960119f.lib.PQCLEAN_MCELIECE6960119F_CLEAN_CRYPTO_CIPHERTEXTBYTES
-
-	_sk = _mceliece6960119f.ffi.from_buffer(sk)
-	assert len(_sk) == _mceliece6960119f.lib.PQCLEAN_MCELIECE6960119F_CLEAN_CRYPTO_SECRETKEYBYTES
-
-	_mceliece6960119f.lib.PQCLEAN_MCELIECE6960119F_CLEAN_crypto_kem_dec(_key, _c, _sk)
+	_mceliece6960119f.lib.PQCLEAN_MCELIECE6960119F_CLEAN_crypto_kem_dec(
+	    _mceliece6960119f.ffi.from_buffer(shared_key),
+	    ciphertext,
+	    sk)
 
 	return bytes(shared_key)
 
