8a1b56a0e1872d7d5c36f44b6c8f453592a09e2d
James Edington Administrator <james.edington@uah.edu>
2024-01-26 16:14:27 -0600

    fixup5

--- setup.py
+++ setup.py
@@ -5,6 +5,24 @@ from setuptools import setup
 from distutils.command.build_ext import build_ext as _build_ext
 from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 
+
+class build_ext(_build_ext):
+    """https://github.com/joerick/python-ctypes-package-sample/blob/7db688cd6ee32ae95bce0f75fb7d806926e20252/setup.py#L11"""
+    def build_extension(self, ext):
+        self._ctypes = isinstance(ext, CTypesExtension)
+        return super().build_extension(ext)
+
+    def get_export_symbols(self, ext):
+        if self._ctypes:
+            return ext.export_symbols
+        return super().get_export_symbols(ext)
+
+    def get_ext_filename(self, ext_name):
+        if self._ctypes:
+            return ext_name + ".so"
+        return super().get_ext_filename(ext_name)
+
+
 class bdist_wheel_abi_none(_bdist_wheel):
     """https://github.com/joerick/python-ctypes-package-sample/blob/7db688cd6ee32ae95bce0f75fb7d806926e20252/setup.py#L29"""
     def finalize_options(self):
@@ -14,6 +32,7 @@ class bdist_wheel_abi_none(_bdist_wheel):
     def get_tag(self):
         python, abi, plat = _bdist_wheel.get_tag(self)
         return "py3", "none", plat
+
 
 setup(
     cmdclass={"build_ext": build_ext, "bdist_wheel": bdist_wheel_abi_none},
