cef5636f801af2266f26fc4b9db59b268c229101
James Edington Administrator <james.edington@uah.edu>
2024-01-31 15:57:51 -0600

    Normalize code

--- pqc/sign/sphincs_shake_128s_simple.py
+++ pqc/sign/sphincs_shake_128s_simple.py
@@ -18,42 +18,34 @@ def keypair():
 
 	errno = _crypto_sign_keypair(_pk, _sk)
 
-	if errno:
-		raise RuntimeError(f"{_crypto_sign_keypair.__name__} returned error code {errno}")
-	return bytes(_pk), bytes(_sk)
+	if errno == 0:
+		return bytes(_pk), bytes(_sk)
+	raise RuntimeError(f"{_crypto_sign_keypair.__name__} returned error code {errno}")
 
 
 def sign(m, sk):
 	_m = ffi.from_buffer(m)
-
 	_sk = ffi.cast(_T_SECRETKEY, ffi.from_buffer(sk))
-	assert len(_sk) == len(sk)
-
 	_sig = ffi.new(_T_SIGNATURE)
 	_siglen = ffi.new('size_t*')
 
 	errno = _crypto_sign_signature(_sig, _siglen, _m, len(m), _sk)
-	assert len(_sig) == _siglen[0]  # Fixed-length signature
 
-	if errno:
-		raise RuntimeError(f"{_crypto_sign_signature.__name__} returned error code {errno}")
-
-	return bytes(_sig)
+	if errno == 0:
+		assert len(_sig) == _siglen[0]  # Fixed-length signature
+		return bytes(_sig)
+	raise RuntimeError(f"{_crypto_sign_signature.__name__} returned error code {errno}")
 
 
 def verify(sig, m, pk):
 	_sig = ffi.cast(_T_SIGNATURE, ffi.from_buffer(sig))
-
 	_m = ffi.from_buffer(m)
-
 	_pk = ffi.cast(_T_PUBLICKEY, ffi.from_buffer(pk))
 
 	errno = _crypto_sign_verify(_sig, len(_sig), _m, len(_m), _pk)
 
-	if errno:
-		if errno == -1:
-			raise ValueError('verification failed')
-		raise RuntimeError(f"{_crypto_sign_verify.__name__} returned error code {errno}")
-
-	return
-
+	if errno == 0:
+		return
+	if errno == -1:
+		raise ValueError('verification failed')
+	raise RuntimeError(f"{_crypto_sign_verify.__name__} returned error code {errno}")
