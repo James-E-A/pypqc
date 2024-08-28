dd833554d6407eebdab528170a9f7f46341d6c1f
James Edington Administrator <codegeek98@gmail.com>
2024-08-28 10:53:07 -0500

    renamed CFFI exposed functions

--- projects/pypqc-cffi-bindings-libre/cffi_modules/mceliece6688128_clean.py
+++ projects/pypqc-cffi-bindings-libre/cffi_modules/mceliece6688128_clean.py
@@ -1,48 +1,7 @@
 #!/usr/bin/env python3
-# This should probably be converted into a Makefile
-
-from collections import deque
-from itertools import chain
-from pathlib import Path
-import platform
-import re
-import subprocess
-
-
-def main():
-	root = Path(__file__).parent.parent  # absolute path to monorepo root, may or may not equal cwd
-	assert (root / 'lib' / 'PQClean').exists()
-	for projdir in (root / 'projects').iterdir():  # absolute path to "project" dir, may or may not equal cwd
-		proj_packageroot = projdir / 'src'
-		(projdir / 'cffi_modules').mkdir(exist_ok=True)
-
-		proj_libdir = projdir / 'lib'
-		if (proj_libdir.is_file() or not proj_libdir.exists()):
-			if platform.system() == 'Windows':
-				# Git failed to check out the symlink properly.
-				# Known bug on Windows systems without Administrator access.
-				# Polyfill the broken/missing checkout.
-				proj_libdir.unlink(missing_ok=True)
-				subprocess.check_call(["MKLINK", "/J", proj_libdir, Path('..', '..', 'lib')], shell=True, cwd=proj_libdir.parent)
-
-		for alg_packagedir in (proj_packageroot / 'pqc' / '_lib').iterdir():
-			package = f'pqc._lib.{alg_packagedir.name}'
-			alg_type, alg_prefix = alg_packagedir.name.split('_')
-			for alg_dir in ((projdir / 'lib' / 'PQClean'
-			                 / f'crypto_{alg_type}').glob(f'{alg_prefix}*')
-			):
-
-				for alg_impl_dir in (p for p in alg_dir.iterdir() if p.is_dir()):  # absolute path to implementation directory
-					if alg_impl_dir.name != 'clean':
-						continue # FIXME
-
-					common_includes = ['fips202', 'randombytes', 'compat', 'crypto_declassify', 'sha2'] # FIXME
-
-					(projdir / 'cffi_modules' / f'{alg_dir.name}_{alg_impl_dir.name}.py').write_text(f"""\
-#!/usr/bin/env python3
-\"\"\"AUTOMATICALLY GENERATED FILE.
+"""AUTOMATICALLY GENERATED FILE.
 RUN make.py IN THE PARENT MONOREPO TO REGENERATE THIS FILE.
-\"\"\"
+"""
 
 # 1. PyPI imports
 import cffi  # https://pypi.org/project/cffi/
@@ -56,10 +15,10 @@ import platform
 import re
 
 # 3. Constants
-IMPL_DIR = Path({', '.join(repr(s) for s in alg_impl_dir.relative_to(projdir).parts)})
+IMPL_DIR = Path('lib', 'PQClean', 'crypto_kem', 'mceliece6688128', 'clean')
 COMMON_DIR = IMPL_DIR / '..' / '..' / '..' / 'common'
-PARENT_PACKAGENAME = {repr('.'.join(s for s in alg_packagedir.relative_to(proj_packageroot).parts))}
-COMMON_INCLUDES = {common_includes!r}  # TODO determine if we can ditch the common directory in favor of e.g. leaning on system libraries?
+PARENT_PACKAGENAME = 'pqc._lib.kem_mceliece'
+COMMON_INCLUDES = ['fips202', 'randombytes', 'compat', 'crypto_declassify', 'sha2']  # TODO determine if we can ditch the common directory in favor of e.g. leaning on system libraries?
 IS_WIN = platform.system() == 'Windows'
 
 # 4. Functions
@@ -69,7 +28,7 @@ def map_immed(f, it, *, splat=False):
 # 5. Body
 ffibuilder = cffi.FFI()
 
-kwextra = {{'py_limited_api': True}}
+kwextra = {'py_limited_api': True}
 
 makefile_parsed = parse_makefile(IMPL_DIR / ('Makefile.microsoft_nmake' if IS_WIN else 'Makefile'))
 libname = Path(makefile_parsed['LIBRARY' if IS_WIN else 'LIB']).stem
@@ -88,43 +47,43 @@ libraries = []
 # Strip off the PQCLEAN_FOO_ prefixes.
 # We do this by cffi-cdef'ing the clean (prefix-free) function names,
 # then c-define'ing the clean function name to transmute it to its prefixed version.
-namespace = re.search(r'(?m)^#define (\\w+)CRYPTO_ALGNAME', api_src).group(1)
-namespace_r = re.compile(rf'({{re.escape(namespace)}}(\\w+))')
-cdef_r = re.compile(r'(?ms)^(?:\\w+ .*?;|#define \\w+[^\\S\\n]+(?=\\S)(?!").*?$)')
-cdef_define_r = re.compile(r'(?<=^#define )(\\w+) (.*)')
-for cdef in (re.sub(cdef_define_r, "\\\\1 ...", m[0]) for m in re.finditer(cdef_r, api_src)):
+namespace = re.search(r'(?m)^#define (\w+)CRYPTO_ALGNAME', api_src).group(1)
+namespace_r = re.compile(rf'({re.escape(namespace)}(\w+))')
+cdef_r = re.compile(r'(?ms)^(?:\w+ .*?;|#define \w+[^\S\n]+(?=\S)(?!").*?$)')
+cdef_define_r = re.compile(r'(?<=^#define )(\w+) (.*)')
+for cdef in (re.sub(cdef_define_r, "\\1 ...", m[0]) for m in re.finditer(cdef_r, api_src)):
 	m = re.search(namespace_r, cdef)
 	if not m:
 		if 'PQCLEAN_FALCONPADDED' in cdef:
 			continue
-	cdefs.append(re.sub(namespace_r, "\\\\2", cdef))
-	c_header_sources.append(f"#define {{m[2]}} {{m[1]}}")
+	cdefs.append(re.sub(namespace_r, "\\2", cdef))
+	c_header_sources.append(f"#define {m[2]} {m[1]}")
 
 
 if 'SOURCES' in makefile_parsed:
 	for source in (Path(IMPL_DIR, s.strip()) for s in makefile_parsed['SOURCES'].split()):
-		if IS_WIN and source.suffix in {{'.s', '.S', '.asm'}}:
+		if IS_WIN and source.suffix in {'.s', '.S', '.asm'}:
 			extra_objects.append(source)
 		else:
 			sources.append(source)
 
 elif 'OBJECTS' in makefile_parsed:
 	for source in chain.from_iterable(IMPL_DIR.glob(Path(s.strip()).with_suffix('.*').name) for s in makefile_parsed['OBJECTS'].split()):
-		if source.suffix in {{'.h'}}:
+		if source.suffix in {'.h'}:
 			depends.append(source)
 			continue
-		if IS_WIN and source.suffix in {{'.s', '.S', '.asm'}}:
+		if IS_WIN and source.suffix in {'.s', '.S', '.asm'}:
 			extra_objects.append(source)
 		else:
 			sources.append(source)
 
 
 for internal_libname in COMMON_INCLUDES:
-	for source in COMMON_DIR.glob(f'{{internal_libname}}*'):
-		if source.suffix in {{'.h'}}:
+	for source in COMMON_DIR.glob(f'{internal_libname}*'):
+		if source.suffix in {'.h'}:
 			depends.append(source)
 			continue
-		if IS_WIN and source.suffix in {{'.s', '.S', '.asm'}}:
+		if IS_WIN and source.suffix in {'.s', '.S', '.asm'}:
 			extra_objects.append(source)
 		else:
 			sources.append(source)
@@ -186,8 +145,8 @@ if platform.system() == 'Windows':
 map_immed(ffibuilder.cdef, cdefs)
 ##map_immed(ffibuilder.include, ffi_includes)  # Not working -- https://github.com/python-cffi/cffi/issues/43
 ffibuilder.set_source(
-	f'{{PARENT_PACKAGENAME}}.{{libname.replace("-", "_")}}',
-	'\\n'.join(c_header_sources),
+	f'{PARENT_PACKAGENAME}.{libname.replace("-", "_")}',
+	'\n'.join(c_header_sources),
 	sources=[p.as_posix() for p in sources],
 	include_dirs=[p.as_posix() for p in include_dirs],
 	extra_objects=[p.as_posix() for p in extra_objects],
@@ -203,9 +162,3 @@ ffi = ffibuilder
 if __name__ == '__main__':
 	import sys
 	ffi.compile(sys.argv[1], verbose=True)
-""")
-
-
-
-if __name__ == '__main__':
-	main()
