ed1a730e18392f5a4373f8b16fe3477731e63abd
James Edington Administrator <codegeek98@gmail.com>
2025-07-25 15:42:21 -0500

    workaround for Git+Windows symlink issue
    https://lore.kernel.org/git/681f8e65b4d3cf82a5920840aa8dc88bde812964.1548771561.git.gitgitgadget@gmail.com/

--- projects/pypqc-cffi-bindings-libre/setup.py
+++ projects/pypqc-cffi-bindings-libre/setup.py
@@ -1,6 +1,18 @@
 from setuptools import setup
 
 from pathlib import Path
+import sys
+if sys.platform == 'win32':
+	import subprocess
+	# For some demented reason, Git doesn't check out
+	# intra-repo directory symlinks as directory junctions
+	# on Windows in non-Developer mode;
+	# this is a workaround for that behavior
+	lib_p = Path(__file__).parent / 'lib'
+	if lib_p.is_file():
+		lib_p_target = Path(lib_p.read_text())
+		lib_p.unlink()
+		subprocess.check_call(["MKLINK", "/J", lib_p, lib_p_target], shell=True)
 
 setup(
 	cffi_modules=[
